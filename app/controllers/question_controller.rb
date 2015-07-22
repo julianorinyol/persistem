0f45fc22e648bab03a9e261b572a0ff4a4042a33
JulianOrinyol <julianorinyol@gmail.com>
2015-07-22 15:09:32 -0400

    Questions controller and some more associations

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,91 @@
+class QuestionController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  # GET /questions
+  # GET /questions.json
+  def index
+    @questions = Question.where(public: true)
+    if current_user
+      @my_questions = Question.where(user_id: current_user.id)
+    end
+    @question = Question.new
+  end
+
+  # GET /questions/1
+  # GET /questions/1.json
+  def show
+  end
+
+  # # GET /questions/new
+  def new
+    @question = Question.new
+  end
+
+  # # GET /questions/1/edit
+  def edit
+    @questions = Question.where(public: true)
+    if current_user
+      @my_questions = Question.where(user_id: current_user.id)
+    end
+  end
+
+  # # POST /questions
+  # # POST /questions.json
+  def create
+    @question = Question.new(question_params)
+    @question.user = current_user
+    respond_to do |format|
+      if @question.save
+        # format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.json { render :show, status: :created, location: @question }
+        # format.html { notice: 'Question was successfully created'}
+      else
+        # format.html { render :new }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
+  # PATCH/PUT /questions/1
+  # PATCH/PUT /questions/1.json
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        # format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { render :show, status: :ok, location: @question }
+      else
+        # format.html { render :edit }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /questions/1
+  # DELETE /questions/1.json
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def question_params
+      params[:question]
+      params.require(:question).permit(:note_id, :text, :subject_id, :user_id)
+    end
+
+end
+=begin
+    t.integer "note_id"
+    t.string  "text"
+    t.integer "subject_id"
+    t.integer "user_id"
+    t.integer "answer_id"  
+=end
