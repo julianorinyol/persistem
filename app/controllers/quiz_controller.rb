40502db979a7869152d9c42cc4bf6e3d3c1f70af
JulianOrinyol <julianorinyol@gmail.com>
2015-08-24 16:19:20 -0400

    made link to create a quiz, wrote controller actions, routes..

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,2 +1,29 @@
 class QuizController < ApplicationController
+  before_action :set_quiz, only: [:show, :edit, :update, :destroy]
+ 
+  def new
+    @quiz = Quiz.create
+    @quiz.get_questions(7)
+    redirect_to quiz_path(id: @quiz.id)
+  end
+
+  def show
+  
+  end
+  
+  def create
+
+  end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_quiz
+      @quiz = Quiz.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def quiz_params
+      params[:question]
+      params.requirquize(:quiz).permit(:note_id, :text, :subject_id, :user_id)
+    end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,8 +1,8 @@
 class Quiz < ActiveRecord::Base
-  # has_many :questions_quiz
-  # has_many :questions
-  # has_many :questions, through: :questions_quiz
   has_and_belongs_to_many :questions
-
   belongs_to :user
+
+  def get_questions num
+    self.questions << Question.take(num)
+  end
 end
