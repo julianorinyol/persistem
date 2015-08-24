965195b24787b0634defe7ce691fe46115963377
JulianOrinyol <julianorinyol@gmail.com>
2015-08-24 02:34:08 -0400

    trying to fix herkoku deploy bug, in hacky way

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 config = YAML.load(ERB.new(File.read('config/evernote.yml')).result)[Rails.env]
-site = config['sandbox'] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
+# site = config['sandbox'] ? 'https://sandbox.evernote.com' : 'https://www.evernote.com'
+
+site = 'https://sandbox.evernote.com'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :evernote, config['consumer_key'], config['consumer_secret'], :client_options => {:site => site}
