3089fd5baaab6817d8521dd41eb7fcad326bb5ba
JulianOrinyol <julianorinyol@gmail.com>
2015-09-01 09:35:36 -0400

    Handled RecordNotFoundError, when notes/show/300   doesn't exist..

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -222,7 +222,9 @@ class NotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = Note.find(params[:id])
+        @note = Note.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        redirect_to notes_path
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -5,45 +5,56 @@ class NotesControllerTest < ActionController::TestCase
     @note = notes(:one)
   end
 
-  test "should get index" do
+ test "should not be able to get index, if not logged in" do
     get :index
-    assert_response :success
-    assert_not_nil assigns(:notes)
+    assert_redirected_to "/sessions/new", "GETTING index, without being logged in, didn't redirect to root_path."
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create note" do
-    assert_difference('Note.count') do
-      post :create, note: {  }
-    end
-
-    assert_redirected_to note_path(assigns(:note))
-  end
-
-  test "should show note" do
+  test "should not be able to show note, if not logged in" do
     get :show, id: @note
-    assert_response :success
-  end
 
-  test "should get edit" do
-    get :edit, id: @note
-    assert_response :success
-  end
-
-  test "should update note" do
-    patch :update, id: @note, note: {  }
-    assert_redirected_to note_path(assigns(:note))
-  end
-
-  test "should destroy note" do
-    assert_difference('Note.count', -1) do
-      delete :destroy, id: @note
-    end
-
-    assert_redirected_to notes_path
+    assert_redirected_to "/sessions/new", "showing note, without being logged in, didn't redirect to root_path."
   end
 end
+# ***************DEFAULT CODE**********************************
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:notes)
+  # end
+
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+
+  # test "should create note" do
+  #   assert_difference('Note.count') do
+  #     post :create, note: {  }
+  #   end
+
+  #   assert_redirected_to note_path(assigns(:note))
+  # end
+
+  # test "should show note" do
+  #   get :show, id: @note
+  #   assert_response :success
+  # end
+
+  # test "should get edit" do
+  #   get :edit, id: @note
+  #   assert_response :success
+  # end
+
+  # test "should update note" do
+  #   patch :update, id: @note, note: {  }
+  #   assert_redirected_to note_path(assigns(:note))
+  # end
+
+  # test "should destroy note" do
+  #   assert_difference('Note.count', -1) do
+  #     delete :destroy, id: @note
+  #   end
+
+  #   assert_redirected_to notes_path
+  # end
