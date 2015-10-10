39791b94c26b31167fa19b787c528eed5d1e42d8
JulianOrinyol <julianorinyol@gmail.com>
2015-10-10 15:20:43 -0700

    wrote steps for answer_quiz feature

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -27,7 +27,6 @@ class QuizController < ApplicationController
   def new_least_answered
     @quiz = Quiz.create(user_id: current_user.id)
     @quiz.add_questions_with_least_answers 7
-    binding.pry
     redirect_to quiz_path(id: @quiz.id)
   end
 
--- features/step_definitions/answer_quiz_steps.rb
+++ features/step_definitions/answer_quiz_steps.rb
@@ -1,23 +1,25 @@
-When(/^I enter "(.*?)" as the answer for the first question$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter "(.*?)" as the answer for the first question$/) do |text|
+  #quiz-show-container > textarea:nth-child(2)
+  Capybara.default_max_wait_time = 7
+  sleep 7
+  first(".quiz-answer-submit-text").set(text)
 end
 
 When(/^I generate a quiz$/) do
-  binding.pry
   click_on("generate-quiz-btn")
-  binding.pry
   click_on("generate_quiz_btn")
-  binding.pry
 end
 
 When(/^I click somewhere else to blur the element$/) do
-  binding.pry
+  x = find_all(".quiz-answer-submit-text")
+  x[2].click()
 end
 
 When(/^I refresh the page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit current_path
 end
 
-Then(/^I can see "(.*?)" on the page$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I can see "(.*?)" on the page$/) do |text|
+  expect(page).to have_content(text)
+  Capybara.default_max_wait_time = 2 
 end
